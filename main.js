be3adccc3031cb00eae5e5bbd287df9fb1c85dae
Jake Garcia <bballwiz4@gmail.com>
2019-12-10 11:30:29 -0800

    Able to get nutrient data next a form for input needs to be created.

--- main.js
+++ main.js
@@ -3,6 +3,12 @@ const APP_ID = "8e4998b8";
 const API_KEY = "adb9b375b1bf447d1ce4f687f4c14ace";
 let API_URL = `https://api.edamam.com/api/food-database/parser?ingr=red%20apple&app_id=${APP_ID}&app_key=${API_KEY}`
 
+function getNutrientData(foodData) {
+    let parsedData = foodData.parsed;
+    let nutrients = parsedData[0].food.nutrients;
+    console.log(nutrients);
+}
+
 fetch(API_URL, {
     method: 'GET',
     body: JSON.stringify(),
@@ -12,4 +18,5 @@ fetch(API_URL, {
     }
 }).then(response => response.json()).then(result => {
     console.log(result);
+    getNutrientData(result);
 })
