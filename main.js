64abb133a4a4957a5e5290fcc80485ae763dba76
Jake Garcia <bballwiz4@gmail.com>
2019-12-09 18:49:00 -0800

    Third party API tested

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 // MAIN.JS
+const APP_ID = "8e4998b8";
+const API_KEY = "adb9b375b1bf447d1ce4f687f4c14ace";
+let API_URL = `https://api.edamam.com/api/food-database/parser?ingr=red%20apple&app_id=${APP_ID}&app_key=${API_KEY}`
+
+fetch(API_URL, {
+    method: 'GET',
+    body: JSON.stringify(),
+    headers: {
+        'content-type' : 'application/json',
+        'Accept' : 'application/json'
+    }
+}).then(response => response.json()).then(result => {
+    console.log(result);
+})
