766845443e5176da16cbc2a39cb7154ee7aec244
Jake Garcia <bballwiz4@gmail.com>
2019-12-12 09:07:58 -0800

    User input handled with default case incorporated, next is form the api_query.

--- main.js
+++ main.js
@@ -1,14 +1,32 @@
 // MAIN.JS
 const APP_ID = "8e4998b8";
 const API_KEY = "adb9b375b1bf447d1ce4f687f4c14ace";
-let API_URL = "https://api.edamam.com/api/food-database/parser";
-var queryString = "";
+let FIRST_HALF_API_URL = "https://api.edamam.com/api/food-database/parser?";
+let SECOND_HALF_API_URL = "&app_id=" + APP_ID + "&app_key=" + API_KEY;
 const formElemet = document.querySelector('form');
-let API_URL = `https://api.edamam.com/api/food-database/parser?ingr=${queryString}&app_id=${APP_ID}&app_key=${API_KEY}`
+// let API_URL = `https://api.edamam.com/api/food-database/parser?ingr=${queryString}&app_id=${APP_ID}&app_key=${API_KEY}`
 
-
+// MARK: - Functions for handling user input and requesting info from the API
 function parseUserInfo() {
     //
+    let foodItem = document.querySelector('#foodItem');
+    let servings = document.querySelector('#servings');
+    if (Number(servings.value) == 0 || servings.value == null) {
+        servings.value = 1;
+    }
+    let userInputDict = {
+        "foodItem" : foodItem.value,
+        "servings" : Number(servings.value)
+    }
+    console.log(userInputDict);
+    formElemet.reset();
+    return userInputDict;
+}
+
+function formAPIQuery() {
+    let userInputDict = parseUserInfo();
+
+
 }
 
 function getNutrientData(foodData) {
@@ -18,14 +36,19 @@ function getNutrientData(foodData) {
     return nutrients;
 }
 
-fetch(API_URL, {
-    method: 'GET',
-    body: JSON.stringify(),
-    headers: {
-        'content-type' : 'application/json',
-        'Accept' : 'application/json'
-    }
-}).then(response => response.json()).then(result => {
-    console.log(result);
-    getNutrientData(result);
-});
+// fetch(API_URL, {
+//     method: 'GET',
+//     body: JSON.stringify(),
+//     headers: {
+//         'content-type' : 'application/json',
+//         'Accept' : 'application/json'
+//     }
+// }).then(response => response.json()).then(result => {
+//     console.log(result);
+//     getNutrientData(result);
+// });
+
+formElemet.addEventListener('submit', (event) => {
+    event.preventDefault();
+    parseUserInfo();
+})
