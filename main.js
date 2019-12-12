a8f6c489cf980908e3b208f1222aa12548d903b5
Jake Garcia <bballwiz4@gmail.com>
2019-12-11 23:33:18 -0800

    Started to write function to handle the user input.

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 // MAIN.JS
 const APP_ID = "8e4998b8";
 const API_KEY = "adb9b375b1bf447d1ce4f687f4c14ace";
-let API_URL = `https://api.edamam.com/api/food-database/parser?ingr=red%20apple&app_id=${APP_ID}&app_key=${API_KEY}`
+let API_URL = "https://api.edamam.com/api/food-database/parser";
+var queryString = "";
+const formElemet = document.querySelector('form');
+let API_URL = `https://api.edamam.com/api/food-database/parser?ingr=${queryString}&app_id=${APP_ID}&app_key=${API_KEY}`
+
+
+function parseUserInfo() {
+    //
+}
 
 function getNutrientData(foodData) {
     let parsedData = foodData.parsed;
     let nutrients = parsedData[0].food.nutrients;
     console.log(nutrients);
+    return nutrients;
 }
 
 fetch(API_URL, {
